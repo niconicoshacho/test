b0896df26b8dfcf1ec325e5d07a5997b81dc8d10
ニコニコ社長 <niconicoshacho@nikonikoshachou-no-iMac.local>
2019-11-23 17:51:15 +0900

    hoge

--- hello.py
+++ hello.py
@@ -1,16 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = "Hello World"
-    return name
+    name = "Hoge"
+    #return name
+    return render_template('hello.html', title='flask test', name=name)
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
-
-## おまじない
+## 
 if __name__ == "__main__":
     app.run(debug=True)
