e9d0c4a294aa2d4c6f09f1bca82b28f329b20614
ニコニコ社長 <niconicoshacho@nikonikoshachou-no-iMac.local>
2019-11-24 18:00:55 +0900

    s

--- schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists results;
-create table results (
-  `id` integer primary key autoincrement,
-  `title` text not null,
-  `data` text not null,
-  `img` text not null,
-  `created` datetime default CURRENT_TIMESTAMP
-);
